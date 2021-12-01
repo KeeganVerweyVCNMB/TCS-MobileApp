c35d165308169011b21869e457aa5098868bc709
KeeganVerweyVCNMB <81228350+KeeganVerweyVCNMB@users.noreply.github.com>
2021-12-01 21:39:15 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/complaintsapp/MainActivity.java
+++ app/src/main/java/com/example/complaintsapp/MainActivity.java
@@ -6,20 +6,27 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
-    private WebView webview;
-
+    private WebView webView;
+    ProgressBar loader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        webview = (WebView) findViewById(R.id.webView);
-
-        webview.setWebViewClient(new WebViewClient());
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setDomStorageEnabled(true);
-        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        webview.loadUrl("https://westwardho.azurewebsites.net/");
+        loader = (ProgressBar)findViewById(R.id.progressBar);
+        loader.setVisibility(RelativeLayout.VISIBLE);
+        webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://westwardho.azurewebsites.net");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        loader.setVisibility(RelativeLayout.INVISIBLE);
+    }
+    public void onBackPressed(){
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
